f010a2c7c039b923ab50c4044f9e4346f4c41471
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-11-27 11:07:52 +0000

    create state machines enumerations

---
+++ src/main/java/guru/springframework/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,9 @@
+package guru.springframework.msscssm.domain;
+
+/**
+ * Created by fabreu on 27/11/2021
+ */
+public enum PaymentEvent {
+
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINE, AUTHORIZE, AUTH_APPROVED, AUTH_DECLINE
+}
